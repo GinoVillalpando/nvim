42ab6a2eaa6db380edb7719d6edaa431b4715788
Gino Villalpando <gino@rs21.io>
2025-02-17 01:25:16 -0700

    formatting bugged me

--- lua/griev/lazy/colors.lua
+++ lua/griev/lazy/colors.lua
@@ -2,8 +2,8 @@ function SetColorScheme(color)
 	color = color or "kanagawa"
 	vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 end
 
